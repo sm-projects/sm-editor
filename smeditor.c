d250d43c638fbaca87e90a18a23581767f9ab892
smazumder05 <smazumder@gmail.com>
2020-12-26 10:17:06 -0500

    getCursor - step32.

--- smeditor.c
+++ smeditor.c
@@ -2,6 +2,7 @@
  *  A simple editor implementation.
  */
 
+#include<sys/ioctl.h>
 #include<unistd.h>
 #include<termios.h>
 #include<stdlib.h>
@@ -12,7 +13,15 @@
 
 #define CTRL_KEY(k)  ((k) & 0x1f)
 
-struct termios orig_termios;
+// A struct to hold edtor configs and state.
+struct editorConfig {
+ //Number of icols and rows in the screen available from ioctl
+ int screen_rows;
+ int screen_cols;
+ struct termios orig_termios;
+};
+
+struct editorConfig editC;
 
 /** All terminal handling functions. ************************************/
 
@@ -22,7 +31,7 @@ struct termios orig_termios;
  *  this function to set the user's terminal back to original state.
  */
 void disableRawMode() {
-    tcsetattr(STDIN_FILENO,TCSAFLUSH, &orig_termios);
+    tcsetattr(STDIN_FILENO,TCSAFLUSH, &editC.orig_termios);
 }
 
 /**
@@ -47,6 +56,60 @@ char editorReadKey() {
     }
     return c;
 }
+
+/**
+ * The n command (Device Status Report) can be used to query the terminal for status
+ * information. We want to give it an argument of 6 to ask for the cursor position.
+ * Then we can read the reply from the standard input.
+ * -----------------------------------------------------------------------------
+ * Let’s print out each character from the standard input to see what the reply
+ * looks like.
+ */
+int getCursorPosition() {
+    if (write(STDOUT_FILENO, "\x1b[6n",4) != 4) return -1;
+
+    printf("\r\n");
+    char c;
+    while(read(STDIN_FILENO,&c,1) == 1) {
+        if(iscntrl(c)){
+            printf("%d\r\n",c);
+        } else {
+            printf("%d ('%c')\r\n", c, c);
+        }
+    }
+    editorReadKey();
+
+    return -1;
+}
+
+/**
+ * Get the size of the terminal by simply calling ioctl() with the TIOCGWINSZ request.
+ * (As far as I can tell, it stands for Terminal IOCtl (which itself stands for
+ * Input/Output Control) Get WINdow SiZe.)
+ * ioctl(), TIOCGWINSZ, and struct winsize come from <sys/ioctl.h>
+ * On success, ioctl() will place the number of columns wide and the
+ * number of rows high the terminal is into the given winsize struct.
+ * On failure, ioctl() returns -1.
+ *
+ * If it succeeded, we pass the values back by setting the int references that
+ * were passed to the function.i
+ */
+int getWindowSize(int *rows, int *cols) {
+    struct winsize ws;
+
+   /* We also check to make sure the values it gave back weren’t 0,
+    * because apparently that’s a possible erroneous outcome.
+    */
+    if( 1 || ioctl(STDOUT_FILENO,TIOCGWINSZ,&ws) == -1 || ws.ws_col == 0) {
+        if(write(STDOUT_FILENO,"\x1b[999C\x1b[999B", 12) != 12) return -1;
+        return getCursorPosition();
+    } else {
+        *cols = ws.ws_col;
+        *rows = ws.ws_row;
+        return 0;
+    }
+}
+
 /** All keyboard input handling functions. ******************************/
 void processKeypress() {
     char c = editorReadKey();
@@ -73,9 +136,17 @@ void processKeypress() {
  *   a number of flags in terminal attributes structure.
  */
 void enableRawMode(){
-    tcgetattr(STDIN_FILENO, &orig_termios);
 
-    struct termios  raw  = orig_termios;
+    if (tcgetattr(STDIN_FILENO, &editC.orig_termios) == -1)
+        handleError("Problem getting terminical config struct.");
+
+    /**
+     * Register disableRawMode as a callback when main exits such that
+     * we can leave everything.
+     */
+    atexit(disableRawMode);
+
+    struct termios  raw  = editC.orig_termios;
     /*
      * ECHO is a bitflag, defined as 00000000000000000000000000001000 in binary.
      * We use the bitwise-NOT operator (~) on this value to get 11111111111111111111111111110111.
@@ -99,19 +170,16 @@ void enableRawMode(){
     raw.c_cc[VMIN] = 0; //set control charecter flags for read() to wait on bytes read, return afterwards
     raw.c_cc[VTIME] = 1; //set cc VTIME to max wait time for read() before it returns
 
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
-    /* Register disableRawMode as a callback when main exits such that we can leave everything
-     * as is afer exit.
-     */
-    atexit(disableRawMode);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) {
+        handleError("Error changing terminal config attributes.");
+    }
 }
-
 
 /**       Editor output functions. *******************************************/
 
 void editorDrawRows() {
     int i;
-    for (i=0;i<24;i++){
+    for (i=0; i<editC.screen_rows; i++){
         write(STDOUT_FILENO, "~\r\n", 3);
     }
 }
@@ -129,9 +197,15 @@ void editorClearScreen() {
     write(STDOUT_FILENO, "\x1b[H", 3); //Repositions the cursor to the first row and col
 }
 
-
+/** Editor init. */
+void initEditor() {
+    if (getWindowSize(&editC.screen_rows, &editC.screen_cols) == -1) {
+        handleError("Unable to get window size.");
+    }
+}
 int main() {
     enableRawMode();
+    initEditor();
 
     // read one byte at a time and quit reading when key pressed is 'q'
     while (1) {
